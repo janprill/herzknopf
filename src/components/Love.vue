0d707a011e2f66f7dc85608581c3173cd94695e5
Jan Prill <Jan.Prill@mspag.com>
2020-04-10 14:37:55 +0200

    wip

---
+++ src/components/Love.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="mt-8 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-20 xl:mt-24">
+    <div class="lg:grid lg:grid-cols-12 lg:gap-8">
+      <div class="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+        <h2 class="mt-1 text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:leading-none sm:text-6xl lg:text-5xl xl:text-6xl">
+          Vielleicht wird es noch lange dauern, bis wir uns wieder berühren können. 
+          Aber sehen können wir uns!
+        </h2>
+      </div>
+
+      <div class="mt-12 relative sm:max-w-lg sm:mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-span-6 lg:flex lg:items-center">
+        <svg class="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-8 scale-75 origin-top sm:scale-100 lg:hidden" width="640" height="784" fill="none" viewBox="0 0 640 784">
+          <defs>
+            <pattern id="4f4f415c-a0e9-44c2-9601-6ded5a34a13e" x="118" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect y="72" width="640" height="640" class="text-gray-50" fill="currentColor" />
+          <rect x="118" width="404" height="784" fill="url(#4f4f415c-a0e9-44c2-9601-6ded5a34a13e)" />
+        </svg>
+        <div class="relative mx-auto w-full rounded-lg shadow-lg lg:max-w-md">
+          <button class="relative block w-full rounded-lg overflow-hidden focus:outline-none focus:shadow-outline">
+            <img class="w-full" src="https://images.unsplash.com/photo-1556740758-90de374c12ad?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="Woman making a sale" />
+            <div class="absolute inset-0 w-full h-full flex items-center justify-center">
+              <svg class="h-20 w-20 text-indigo-500" fill="currentColor" viewBox="0 0 84 84">
+                <circle opacity="0.9" cx="42" cy="42" r="42" fill="white" />
+                <path d="M55.5039 40.3359L37.1094 28.0729C35.7803 27.1869 34 28.1396 34 29.737V54.263C34 55.8604 35.7803 56.8131 37.1094 55.9271L55.5038 43.6641C56.6913 42.8725 56.6913 41.1275 55.5039 40.3359Z" />
+              </svg>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,12 +3,22 @@
 
     <hero />
 
-    <section class="mt-32">
-      <h2>So funktioniert es:</h2>
-      <step />
-      <step />
-      <step />
-    </section>
+    <main class="max-w-screen-xl mx-auto">
+      <love />
+
+      <section class="mt-20 max-w-2xl">
+        <p class="">
+          Wir haben hochmoderne Videokonferenzsysteme in Pflegeheime gebracht, damit Sie unkompliziert und sicher mit  Ihren Angehörigen im Pflegeheim Kontakt aufnehmen können.
+        </p>
+      </section>
+
+      <section class="mt-32">
+        <h2>So funktioniert es:</h2>
+        <step />
+        <step />
+        <step />
+      </section>
+    </main>
 
     <footer />
 
@@ -16,15 +26,17 @@
 </template>
 
 <script>
-import Step from "~/components/Step.vue"
 import Hero from "~/components/Hero.vue"
+import Love from "~/components/Love.vue"
+import Step from "~/components/Step.vue"
 export default {
   components: {
     Hero,
+    Love,
     Step,
   },
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'Herzknopf'
   }
 }
 </script>
