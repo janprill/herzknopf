7be3b730bd92849f2fc2e9a558ccb177ef913562
Jan Prill <Jan.Prill@mspag.com>
2020-04-12 12:35:24 +0200

    skew content back

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,8 +2,11 @@
   <Layout>
     <div>
       <div class="relative -mt-48">
-        <div class="absolute skew-y-3 w-full bg-gray-300 h-96 transform top-0" style="background: linear-gradient(316.21deg, #690202 2.76%, #A42626 116.62%);">
+        <div class="absolute skew-y-3 w-full bg-gray-300 h-auto transform top-0" style="background: linear-gradient(316.21deg, #690202 2.76%, #A42626 116.62%);">
           Background
+          <div class="-skew-y-3 mt-48 p-24 transform max-w-xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dignissimos iste, itaque voluptas adipisci facere quo magni quis praesentium numquam laudantium hic quas quaerat sequi iure tenetur reiciendis voluptatum similique?
+          </div>
         </div>
         <div class="absolute">
           Content
