d0c7a6cb929b6a28b7ac91313267cb07e12b2562
Jan Prill <Jan.Prill@mspag.com>
2020-04-12 12:46:14 +0200

    skew layout (wip)

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,15 +1,11 @@
 <template>
   <Layout>
-    <div>
+    <div class="container mx-auto min-h-screen">
       <div class="relative -mt-48">
         <div class="absolute skew-y-3 w-full bg-gray-300 h-auto transform top-0" style="background: linear-gradient(316.21deg, #690202 2.76%, #A42626 116.62%);">
-          Background
           <div class="-skew-y-3 mt-48 p-24 transform max-w-xl">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet dignissimos iste, itaque voluptas adipisci facere quo magni quis praesentium numquam laudantium hic quas quaerat sequi iure tenetur reiciendis voluptatum similique?
           </div>
-        </div>
-        <div class="absolute">
-          Content
         </div>
       </div>
     </div>
